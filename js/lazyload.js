246009a5907639f2d54276e7aab267b569cbe1e6
yyj <skyblueice234@gmail.com>
2020-02-22 18:17:41 +0800

    Site updated: 2020-02-22 18:17:38

--- js/lazyload.js
+++ js/lazyload.js
@@ -1,1 +1,49 @@
-!function(o){var i=Array.prototype.slice.call(document.querySelectorAll("img[srcset]"));function n(){for(var r=0;r<i.length;r++)t=i[r],0,e=t.getBoundingClientRect(),n=o.innerHeight||document.documentElement.clientHeight,0<=e.top&&0<=e.left&&e.top<=3*n&&function(){var t,e,n,o,c=i[r];t=c,e=function(){i=i.filter(function(t){return c!==t})},n=new Image,o=t.getAttribute("src"),n.onload=function(){t.srcset=o,e&&e()},n.srcset=o}();var t,e,n}n(),o.addEventListener("scroll",function(){var t,e;t=n,e=o,clearTimeout(t.tId),t.tId=setTimeout(function(){t.call(e)},100)})}(void 0);
+(function (window) {
+	var images = Array.prototype.slice.call(document.querySelectorAll('img[srcset]'));
+
+	function elementInViewport(el) {
+		var rect = el.getBoundingClientRect();
+		var height = window.innerHeight || document.documentElement.clientHeight;
+		return (
+			rect.top >= 0
+			&& rect.left >= 0
+			&& rect.top <= height * 3
+		);
+	}
+	function loadImage(el, fn) {
+		var img = new Image(), src = el.getAttribute('src');
+		img.onload = function () {
+			el.srcset = src;
+			fn ? fn() : null;
+		};
+		img.srcset = src;
+	}
+
+	function processImages() {
+		for (var i = 0; i < images.length; i++) {
+			if (elementInViewport(images[i])) {
+				(function(index){
+					var loadingImage = images[index];
+					loadImage(loadingImage, function () {
+						images = images.filter(function(t) {
+							return loadingImage !== t;
+						});
+					});
+				})(i);
+			}
+		}
+	}
+
+	function throttle(method, context) {
+		clearTimeout(method.tId);
+		method.tId = setTimeout(function () {
+				method.call(context);
+		}, 100);
+	}
+
+	processImages();
+
+	window.addEventListener('scroll', function () {
+		throttle(processImages, window);
+	});
+})(this);
